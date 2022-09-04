b977bf7205bfa7a2e19ca447bb6082c816f34413
ravinu <ravinu.gcom@gmail.com>
2022-09-04 07:08:06 +0530

    change working dir and print working dir

--- main.sh
+++ main.sh
@@ -2,7 +2,15 @@
 isroot="$(id -u)"
 zero="0"
 if [ $isroot == $zero ]; then
-    echo Nice im root
+    (cd /var/www/html/admin/scripts/pi-hole/php && pwd)
+    read newfile1
+    if [ -f "$newfile1" ]; then
+        echo "File is found"
+    else
+        echo "File is not found"
+fi
+
+
 else
     echo Make me root
 fi
