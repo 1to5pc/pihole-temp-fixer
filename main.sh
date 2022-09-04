dc653beb1e465745e09aa31549a8e55f11411944
ravinu <ravinu.gcom@gmail.com>
2022-09-04 07:17:04 +0530

    extend brackets

--- main.sh
+++ main.sh
@@ -2,14 +2,14 @@
 isroot="$(id -u)"
 zero="0"
 if [ $isroot == $zero ]; then
-    (cd /var/www/html/admin/scripts/pi-hole/php && pwd)
+    (cd /var/www/html/admin/scripts/pi-hole/php && pwd
     read newfile1
     if [ -f "$newfile1" ]; then
         echo "File is found"
     else
         echo "File is not found"
 fi
-
+    )
 
 else
     echo Make me root
